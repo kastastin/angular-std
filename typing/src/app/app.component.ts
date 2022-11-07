a4d9d598bfd89a9526fe74544506c90a0a694870
kastastin <kostya242421@gmail.com>
2022-11-07 16:57:20 +0200

    initial commit

---
+++ typing/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { faker } from "@faker-js/faker";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  test = faker.git.commitMessage();
+}
