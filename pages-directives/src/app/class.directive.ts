4a050d96eba46d5bed3c2588e62e3a5c5737345c
kastastin <kostya242421@gmail.com>
2022-11-09 01:50:59 +0200

    change ngClass directive on custom one

--- pages-directives/src/app/class.directive.ts
+++ pages-directives/src/app/class.directive.ts
@@ -4,13 +4,15 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[appClass]'
 })
 export class ClassDirective {
-  @Input() backgroundColor = ''; 
+  constructor(private element: ElementRef) {}
 
-  constructor(private element: ElementRef) {
-    // NEVER DO THIS!!!
-    setTimeout(() => {
-      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
-    }, 50);
+  @Input('appClass') set classNames(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
-
 }
