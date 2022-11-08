b14918861ac8effae122d5c34a33f1d9450d11c6
kastastin <kostya242421@gmail.com>
2022-11-08 13:53:30 +0200

    finished

--- payment-pipes/src/app/app.component.ts
+++ payment-pipes/src/app/app.component.ts
@@ -7,8 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = '';
+  date = '';
+  amount = 0;
+  height = 0;
+  miles = 0;
   
   onNameChange(event: any) {
     this.name = event.target.value;
+  }
+
+  onDateChange(event: any) {
+    this.date = event.target.value;
+  }
+
+  onAmountChange(event: any) {
+    this.amount = parseFloat(event.target.value);
+  }
+
+  onHeightChange(event: any) {
+    this.height = parseFloat(event.target.value);
+  }
+
+  onMilesChange(event: any) {
+    this.miles = parseFloat(event.target.value);
   }
 }
