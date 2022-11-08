b2e40818d8186bd457aecde907c64cbec60ba81a
kastastin <kostya242421@gmail.com>
2022-11-08 22:58:57 +0200

    images + ng-container

--- pages-directives/src/app/app.component.ts
+++ pages-directives/src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   currentPage = 0;
 
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
+
   images = [
     {
       title: 'At the Beach',
@@ -24,6 +28,26 @@ export class AppComponent {
     {
       title: 'At the city',
       url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the river',
+      url: 'https://images.unsplash.com/photo-1519852476561-ec618b0183ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cml2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the forest',
+      url: 'https://images.unsplash.com/photo-1542202229-7d93c33f5d07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the lake',
+      url: 'https://images.unsplash.com/photo-1583244685026-d8519b5e3d21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the market',
+      url: 'https://images.unsplash.com/photo-1501523460185-2aa5d2a0f981?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFya2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the jungle',
+      url: 'https://images.unsplash.com/photo-1599998372764-ac8cbb604f21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGp1bmdsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
     }
   ];
 }
